9fbe93d8d1572fc85142b5b7ac757a67ba50f4fc
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-30 11:13:32 -0700

    Add files via upload

---
+++ Super Keyword/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hero hero1 = new Hero("Batman", 42, "Money");
+		//System.out.println(hero1.name);
+		//System.out.println(hero1.age);
+		//System.out.println(hero1.power);
+		System.out.println(hero1.toString());
+	}
+
+}
